26effb457cd44226a66074e8b2eac10e5cba519c
habibi55 <muhamadhabibi14168@gmail.com>
2021-09-15 21:36:31 +0700

    Time is Endless

---
+++ Endless-Run/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")] 
+    public float moveAccel;
+    public float maxSpeed;
+
+    private Rigidbody2D rig;
+
+    [Header("Jump")] 
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    [Header("Ground Raycast")] 
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    private Animator anim;
+
+    private CharacterSoundController sound;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("Game Over")] 
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    [Header("Camera")] 
+    public CameraMoveController gameCamera;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    private void Update()
+    {
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+                
+                sound.PlayJump();
+            }
+        }
+        
+        // ubah animasi
+        anim.SetBool("isOnGround", isOnGround);
+        
+        // kalkulasi skor
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        // cek game over
+        if (transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        // set high score
+        score.FinishScoring();
+        
+        // stop pergerakan kamera
+        gameCamera.enabled = false;
+        
+        // tunjukkan panel game over
+        gameOverScreen.SetActive(true);
+        
+        // disable karakter
+        enabled = false;
+    }
+
+    private void FixedUpdate()
+    {
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+            groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        
+        // kalkulasi velocity vector
+        Vector2 vectorVelocity = rig.velocity;
+
+        if (isJumping)
+        {
+            vectorVelocity.y += jumpAccel;
+            isJumping = false;
+        }
+        
+        vectorVelocity.x = Mathf.Clamp(vectorVelocity.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = vectorVelocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), 
+            Color.white);
+    }
+}
